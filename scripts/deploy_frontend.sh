68de3b876a39807507410e148b450f7751f659a6
Jehu Shalom Amanna <jehu.amanna@quillbot.com>
2023-07-10 23:04:13 +0530

    fix docker

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 branch="main"
 >~/.ssh/known_hosts
-ssh -i "cli.pem" ec2-user@ec2-16-171-10-49.eu-north-1.compute.amazonaws.com -o "StrictHostKeyChecking no" -t "cd hiring-jehu-amanna-flashcards-ui && git reset --hard && git fetch && git checkout $branch && git reset --hard @{u} &&  git pull && docker build -f Dockerfile.frontend -t frontend .  && docker stop frontend || true && docker container prune -f && docker run -d --restart unless-stopped --name frontend -p 4200:4200 -d frontend && docker system prune -f "
+ssh -i "cli.pem"  ubuntu@ec2-16-170-215-166.eu-north-1.compute.amazonaws.com -o "StrictHostKeyChecking no" -t "cd hiring-jehu-amanna-flashcards-ui && git reset --hard && git fetch && git checkout $branch && git reset --hard @{u} &&  git pull && docker build -f Dockerfile.frontend -t frontend .  && docker stop frontend || true && docker container prune -f && docker run -d --restart unless-stopped --name frontend -p 4200:4200 -d frontend && docker system prune -f "
