0b94345f139fda361c9cbfc30d9ee3681e4c2cb3
Jehu Shalom Amanna <jehu.amanna@quillbot.com>
2023-07-11 15:06:52 +0530

    80:8080

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -5,4 +5,4 @@ scp -i "cli.pem" -r  ./build.tar.gz   ubuntu@ec2-16-170-215-166.eu-north-1.compu
 
 branch="main"
 >~/.ssh/known_hosts
-ssh -i "cli.pem"  ubuntu@ec2-16-170-215-166.eu-north-1.compute.amazonaws.com -o "StrictHostKeyChecking no" -t "tar xvf build.tar.gz && cd hiring-jehu-amanna-flashcards-ui && git reset --hard && git fetch && git checkout $branch && git reset --hard @{u} &&  git pull && rm -rf ./build && mv ../build . &&  docker build -f Dockerfile.frontend -t frontend .  && docker stop frontend || true && docker container prune -f && docker run -d --restart unless-stopped --name frontend -p 8080:8080 -d frontend && docker system prune -f "
+ssh -i "cli.pem"  ubuntu@ec2-16-170-215-166.eu-north-1.compute.amazonaws.com -o "StrictHostKeyChecking no" -t "tar xvf build.tar.gz && cd hiring-jehu-amanna-flashcards-ui && git reset --hard && git fetch && git checkout $branch && git reset --hard @{u} &&  git pull && rm -rf ./build && mv ../build . &&  docker build -f Dockerfile.frontend -t frontend .  && docker stop frontend || true && docker container prune -f && docker run -d --restart unless-stopped --name frontend -p 80:8080 -d frontend && docker system prune -f "
